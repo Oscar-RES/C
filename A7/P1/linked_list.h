0d0bbb0997db374afc9aa839dceb6709355611ea
OSCAR ECHEVERRIA <75795753+Oscar-RES@users.noreply.github.com>
2021-02-09 22:41:15 +0100

    Delete A7 directory

--- A7/P1/linked_list.h
+++
@@ -1,9 +0,0 @@
-struct list {
-    int info;
-    struct list *next;
-};
-void print_list(struct list* my_list);//prints list
-struct list* push_front(struct list *my_list, int value);//adds number to front
-struct list* push_back(struct list* my_list, int value);//adds number to back
-struct list* deletelem(struct list * my_list);//deletes first element
-void dispose_list(struct list* my_list); //nukes the list
